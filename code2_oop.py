edbe4adddba018c02d456de09cc69240aa65b0b7
Amogu J. Uduka <johnsonamogu@gmail.com>
2024-03-06 16:31:17 -0500

    Added new features like def __repr__()

--- code2_oop.py
+++ code2_oop.py
@@ -1,21 +1,30 @@
 class Item:
-
+    pay_rate = 0.8 #The pay rate after 20% discount
+    all = []
     def __init__(self, name: str, price: float, quantity=0):
-        print(f"An Instance created: {name}")
+        # Run validations to the received arguments
+        assert quantity >= 0,("Your quantity cannot be less than zero")
+        assert price > 0, ("Your price has to be greater than 0")
+
+        #Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
+        Item.all.append(self)
 
     def calculate_total_price(self):
-        assert self.quantity >= 0,("Your quantity cannot be less than zero")
-        assert self.price > 0, ("Your price has to be greater than 0")
         return self.price * self.quantity
     
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+    
+    def __repr__(self):
+        return f"Item('{self.name}', {self.price},{self.quantity})"
 
-item1 = Item("Johnson", 3400)
-print(item1.name)
-print("$" + str(item1.calculate_total_price()))
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 3)
+item5 = Item("Keyboard", 75, 5)
 
-item2 = Item("Olive", 3440, 90)
-print(item2.name)
-print("$" + str(item2.calculate_total_price()))
+print(Item.all) #is equivalent to print(repr(Item.all))
