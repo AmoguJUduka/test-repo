315e861f566c7f3e829db2e863840b1c783d2c66
Amogu J. Uduka <johnsonamogu@gmail.com>
2024-03-15 01:38:45 -0400

    Polymorphism in action

--- oop_in_py5.py
+++ oop_in_py5.py
@@ -3,15 +3,17 @@ class AudioFile:
         if not filename.endswith(self.ext):
             raise Exception("Invalid Format")
         self.filename = filename
-        
-
+    
+   
 class MP3(AudioFile):
     ext = "mp3"
     def play(self):
-        print("Playing an MP3 file")
+        print(f"Playing a {self.ext} MP3 file")
 
 class ogg(AudioFile):
     ext = "ogg"
     def play(self):
         print("Playing ogg file")
 
+b = MP3("music.mp3")
+b.play()
